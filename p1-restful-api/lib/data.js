29c91e2628b5bd042cc4d558f166f414c7472638
Kareem Mohllal <kareem.mohllal@gmail.com>
2019-02-04 00:43:19 +0200

    Make simple lib for manipulating data to the file system

---
+++ p1-restful-api/lib/data.js
@@ -0,0 +1,113 @@
+var fs = require('fs');
+var path = require('path');
+
+var lib = {};
+
+// define base directory of the data folder
+lib.baseDir = path.join(__dirname, '../.data/');
+
+// write data to a file
+lib.create = function (dir, file, data, callback) {
+    var filePath = lib.baseDir + dir + '/' + file + '.json';
+    
+    // open file for writing
+    fs.open(filePath, 'wx', function (err, fileDescriptor) {
+        if (!err && fileDescriptor) {
+            // convert data to plain string
+            var dataString = JSON.stringify(data);
+
+            // write to file
+            fs.writeFile(fileDescriptor, dataString, function (err) {
+                if (!err){
+                    // close file
+                    fs.close(fileDescriptor, function (err) {
+                        if (!err){
+                            callback(false);
+                        }
+                        else {
+                            callback('Error closing new file');
+                        }
+                    });
+                }
+                else {
+                    callback('Error writing to new file');
+                }
+            });
+        }
+        else {
+            callback('Error creating new file');
+        }
+    });
+};
+
+// read data from a file
+lib.read = function (dir, file, callback) {
+    var filePath = lib.baseDir + dir + '/' + file + '.json'
+    
+    // read from file
+    fs.readFile(filePath, 'utf8', function (err, data) {
+        callback(err, data);
+    });  
+};
+
+// update data in a file
+lib.update = function (dir, file, data, callback) {
+    var filePath = lib.baseDir + dir + '/' + file + '.json';
+    
+    // open file for updating
+    fs.open(filePath, 'r+', function (err, fileDescriptor) {
+        if (!err && fileDescriptor) {
+            // convert data to plain string
+            var dataString = JSON.stringify(data);
+
+            // truncate file
+            fs.ftruncate(fileDescriptor, function (err) {
+                if (!err){
+                    // write to file
+                    fs.writeFile(fileDescriptor, dataString, function (err) {
+                        if (!err){
+                            // close file
+                            fs.close(fileDescriptor, function (err) {
+                                if (!err){
+                                    callback(false);
+                                }
+                                else {
+                                    callback('Error closing file');
+                                }
+                            });
+                        }
+                        else {
+                            callback('Error writing to file');
+                        }
+                    });
+                }
+                else {
+                    callback('Error truncating file');
+                }
+            });
+
+
+        }
+        else {
+            callback('Error updating file');
+        }
+    });
+};
+
+// delete a file
+lib.delete = function (dir, file, callback) {
+    var filePath = lib.baseDir + dir + '/' + file + '.json';
+    
+    // unlink file
+    fs.unlink(filePath, function (err) {
+        if (!err){
+            callback(false);
+        }
+        else {
+            callback('Error deleting file');
+        }
+    });
+};
+
+// export the module
+module.exports = lib;
