271a418cb7d38563106e87843dfccbd7c7b7af5a
Kareem Mohllal <kareem.mohllal@gmail.com>
2019-02-07 21:33:35 +0200

    Add the checks GET, PUT, and DELETE handlers

--- p1-restful-api/handlers/checks.js
+++ p1-restful-api/handlers/checks.js
@@ -1,14 +1,65 @@
 var _data = require('../lib/data');
 var helpers = require('../lib/helpers');
 var config = require('../config');
+var authHandler = require('./auth');
 
 // container for the checks sub-handlers
 var checksHandler = {};
 
-// TODO:
 // checks - GET
+// required data: check id
+//optional data: none
 checksHandler.GET = function (data, callback) {
-    
+    // check the request query object for the check id
+    var id = typeof(data.queryStringObject.id) == 'string' && data.queryStringObject.id.trim().length > 20? data.queryStringObject.id : false;
+    if (id) {
+        // get the token id from the headers
+        var token = typeof(data.headersObject.token) == 'string'? data.headersObject.token : false;
+        
+        if (token) {
+            // verify that the given token is valid
+            _data.read('tokens', token, function (err, tokenData) {
+                if (!err && tokenData) {
+                    // get the user phone from the token object
+                    var phone = tokenData.phone;
+
+                    // check if the check belong to the provided user
+                    _data.read('users', phone, function (err, userData) {
+                        if (!err && userData) {
+                            if (userData.checks.indexOf(id) > -1) {
+                                // lookup the check
+                                _data.read('checks', id, function (err, checkData){
+                                    if (!err && checkData) {
+                                        callback(200, checkData);
+                                    }
+                                    else {
+                                        console.log(err);
+                                        callback(500, {'Error': 'Could not read the check data'});
+                                    }
+                                });
+                            }
+                            else {
+                                callback(404);
+                            }
+                        }
+                        else {
+                            callback(403);
+                        }
+                    });
+
+                }
+                else {
+                    callback(403, {'Error': 'Invalid token'});
+                }
+            });
+        }
+        else {
+            callback(400, {'Error': 'Missing required headers'});
+        }
+    }
+    else {
+        callback(400, {'Error': 'Missing required fields'});
+    } 
 };
 
 // checks - POST
@@ -100,16 +151,124 @@ checksHandler.POST = function (data, callback) {
     }
 };
 
-// TODO:
 // checks - PUT
+// required data: check id
+// optional data: protocol, url, method, successCodes, or timeoutSeconds (one must be provided)
 checksHandler.PUT = function (data, callback) {
-    
+    // check for required field
+    var id = typeof(data.payload.id) == 'string' && data.payload.id.trim().length > 20 ? data.payload.id.trim() : false;
+
+    // check for optional fields
+    var protocol = typeof(data.payload.protocol) == 'string' && ['https','http'].indexOf(data.payload.protocol) > -1 ? data.payload.protocol : false;
+    var url = typeof(data.payload.url) == 'string' && data.payload.url.trim().length > 0 ? data.payload.url.trim() : false;
+    var method = typeof(data.payload.method) == 'string' && ['post','get','put','delete'].indexOf(data.payload.method) > -1 ? data.payload.method : false;
+    var successCodes = typeof(data.payload.successCodes) == 'object' && data.payload.successCodes instanceof Array && data.payload.successCodes.length > 0 ? data.payload.successCodes : false;
+    var timeoutSeconds = typeof(data.payload.timeoutSeconds) == 'number' && data.payload.timeoutSeconds % 1 === 0 && data.payload.timeoutSeconds >= 1 && data.payload.timeoutSeconds <= 5 ? data.payload.timeoutSeconds : false;
+
+    // error if id is invalid
+    if(id){
+        // error if nothing is sent to update
+        if(protocol || url || method || successCodes || timeoutSeconds){
+            // lookup the check
+            _data.read('checks', id, function (err, checkData) {
+                if (!err && checkData){
+                    var phone = checkData.phone;
+
+                    // get the token id from the headers
+                    var token = typeof(data.headersObject.token) == 'string'? data.headersObject.token : false;
+                    
+                    // verify that the given token is valid and belongs to the user who created the check
+                    authHandler.verifyToken(token, phone, function (err) {
+                        if(!err){
+                            // update check data where necessary
+                            if (protocol) {
+                                checkData.protocol = protocol;
+                            }
+                            if (url) {
+                                checkData.url = url;
+                            }
+                            if (method) {
+                                checkData.method = method;
+                            }
+                            if (successCodes) {
+                                checkData.successCodes = successCodes;
+                            }
+                            if (timeoutSeconds) {
+                                checkData.timeoutSeconds = timeoutSeconds;
+                            }
+                            
+                            // Store the new updates
+                            _data.update('checks', id, checkData, function (err) {
+                                if(!err){
+                                    callback(200);
+                                } else {
+                                    callback(500, {'Error': 'Could not update the check'});
+                                }
+                            });
+                        } 
+                        else {
+                            callback(403);
+                        }
+                    });
+                } 
+                else {
+                    callback(404);
+                }
+            });
+            }
+            else {
+                callback(400, {'Error': 'Missing required fields'});
+            }
+    } else {
+        callback(400, {'Error': 'Missing required field'});
+    }
 };
 
-// TODO:
 // checks - DELETE
+// required data: check id
+// optional data: none
 checksHandler.DELETE = function (data, callback) {
-    
+     // check the request query object for the check id
+    var id = typeof(data.queryStringObject.id) == 'string' && data.queryStringObject.id.trim().length > 20 ? data.queryStringObject.id.trim() : false;
+
+    if (id) {
+        // lookup the check
+        _data.read('checks', id, function (err, checkData) {
+            if (!err && checkData) {
+                var phone = checkData.phone;
+
+                // get the token id from the headers
+                var token = typeof(data.headersObject.token) == 'string'? data.headersObject.token : false;
+                if (token) {
+                    // verify that the given token is valid for a given phone
+                    authHandler.verifyToken(token, phone, function (err) {
+                        if (!err) {
+                            _data.delete('checks', id, function (err) {
+                                if (!err) {
+                                    callback(200);
+                                }
+                                else {
+                                    callback(500, {'Error': 'Could not delete the check'});
+                                }
+                            });
+                        }
+                        else {
+                            callback(403, {'Error': 'Invalid token'});
+                        }
+                    });
+                }
+                else {
+                    callback(400, {'Error': 'Missing required headers'});
+                }
+            }
+            else {
+                callback(404);
+            }
+        });
+    }
+    else {
+        callback(400, {'Error': 'Missing required fields'});
+    }
 };
 
 // export the module
