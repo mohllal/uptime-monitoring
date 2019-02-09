33faf0c14cde220e1ef8492b21ead5dbd099a823
Kareem Mohllal <kareem.mohllal@gmail.com>
2019-02-09 17:21:30 +0200

    Add the basic logic for serving HTML templates

--- handlers/gui/index.js
+++ handlers/gui/index.js
@@ -1,8 +1,24 @@
+var helpers = require('../../lib/helpers');
+
 // container for the index sub-handlers
 var indexHandler = {};
 
 indexHandler.index = function(data, callback) {
-    callback(undefined, undefined, 'html');
+    // handle GET method and refuse any other method
+    if (data.method == 'GET') {
+        // read in a template as a string
+        helpers.getTemplate('index', function (err, str) {
+            if (!err && str) {
+                callback(200, str, 'html');
+            }
+            else {
+                callback(500, undefined, 'html');
+            }
+        });
+    }
+    else {
+        callback(405, undefined, 'html');
+    }
 };
 
 // export the module
